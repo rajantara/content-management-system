e1bbd1f7df51937bd29b892ec4e09ad7a3959559
rajantara <rajanpensas@gmail.com>
2020-08-21 00:23:27 +0800

    add logout

--- routes/users.js
+++ routes/users.js
@@ -164,6 +164,32 @@ router.post('/check', function (req, res, next) {
 
 //====================== logout
 
+router.get('/logout', function (req, res, next) {
+  let token = req.header('token')
+  let response = {
+    logout: false
+  }
+
+  if (!token) {
+    res.status(500).json(response);
+  } else {
+    const decode = jwt.verify(token, secret)
+    console.log('decode', decode);
+    User.findOneAndUpdate({ email: decode.email}, { token: ""}, { new: true})
+      .then(result => {
+
+        console.log(result)
+
+        response.logout = true
+        res.status(200).json(response)
+      })
+      .catch(err => {
+        res.status(500).json(response)
+      })
+  }
+})
+
+
 
 
 module.exports = router;
