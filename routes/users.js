2e6f8c8cec3d98624860e75efcfef872df7f405d
rajantara <rajanpensas@gmail.com>
2020-08-20 23:54:00 +0800

    add check for token

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,14 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 var jwt = require('jsonwebtoken');
+var bcrypt = require('bcrypt');
 const secret = 'secret';
 const {
   response
 } = require('express');
 
 
-//========================= GET users
+//====================== GET users
 router.get('/', function (req, res) {
   let response = [];
 
@@ -36,7 +37,7 @@ router.get('/', function (req, res) {
 
 //====================== register
 router.post('/register', function (req, res, next) {
-  let { 
+  let {
     email,
     password,
     retypepassword
@@ -52,15 +53,15 @@ router.post('/register', function (req, res, next) {
     message: "password doesn't match"
   })
   User.findOne({
-      email
-    })
+    email
+  })
     .then(result => {
       if (result) {
         response.message = 'email already exist';
         console.log('email ditemukan', result);
         return res.status(200).json(response)
       } else {
-        var token = jwt.sign({email: email}, secret);
+        var token = jwt.sign({ email: email }, secret);
         let user = new User({
           email: email,
           password: password,
@@ -83,5 +84,86 @@ router.post('/register', function (req, res, next) {
       })
     })
 });
+
+
+
+//====================== login
+
+router.post('/login', function (req, res, next) {
+  let {
+    email,
+    password
+
+  } = req.body;
+  let response = {
+    message: "",
+    data: {},
+    token: ""
+  }
+
+  User.findOne({ email })
+    .then(data => {
+
+      console.log('data1', data);
+
+      bcrypt.compare(password, data.password)
+        .then(isPasswordTrue => {
+          if (isPasswordTrue) {
+            if (data.token) {
+              response.token = data.token;
+              response.data.email = email;
+              response.data.message = 'login succes dude';
+              res.status(201).json(response)
+            }
+
+          } else {
+            response.message = " authentication failed";
+            res.status(200).json(response);
+          }
+        })
+        .catch(err => {
+          response.message = "Authentication failed dude";
+          res.status(500).json(response);
+        })
+    })
+    .catch(err => {
+      response.message = "email doesn't match dude";
+      res.status(200).json(response);
+    })
+});
+
+
+
+
+//====================== check db
+
+router.post('/check', function (req, res, next) {
+  let token = req.header('token')
+  let response = {
+    valid: false
+  }
+  console.log(token)
+
+  if (!token) {
+    res.status(500).json(response)
+  } else {
+    const decode = jwt.verify(token, secret);
+    console.log(decode)
+    User.find({ email: decode.email})
+    .then(result => {
+      response.valid = true
+      res.status(200).json(response)
+
+    })
+    .catch(err => {
+      res.status(500).json({ response})
+    })
+  }
+})
+
+
+//====================== logout
+
+
 
 module.exports = router;
